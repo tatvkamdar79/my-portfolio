402b8eaaf6b2b3988b77dd862a8fc06a7a87d942
tatvkamdar79 <kamdartatv79@gmail.com>
2022-12-25 01:15:20 +0530

    minor refactor

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,7 +27,7 @@ const Contact = () => {
 
         <div className=" flex justify-center items-center">
           <form
-            action="https://getform.io/f/61c99527-2b15-42cf-9b55-ad37d2f7daa6"
+            action="https://getform.io/f/cc711be4-20b0-48aa-9586-1feb4cf3152e"
             method="POST"
             className=" flex flex-col w-full md:w-1/2"
           >
